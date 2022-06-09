ceead9a08d568e73a3c3521a5f41f294873c40cf
Phenomfunbi <62062746+phenomfunbi@users.noreply.github.com>
2022-06-10 07:23:01 +0800

    Update settings.py

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+2dk=mfa!^2)ae%7)2p55^#odn%a)j)ha)zdws8-=2@x^ai9#y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
